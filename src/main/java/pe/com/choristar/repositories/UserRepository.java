9462098d9a96f034cd88fcc46abf03e1b7095786
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-26 18:46:51 -0500

    feat: agregacion de un repository de usuarios

---
+++ src/main/java/pe/com/choristar/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package pe.com.choristar.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import pe.com.choristar.models.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
